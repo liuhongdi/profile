0d7a434030fdaa3b2a2b73689e95792eac078316
liuhongdi <liuhongdi@gmail.com>
2020-09-23 12:00:10 +0800

    add profile

--- src/main/java/com/profile/demo/controller/HomeController.java
+++ src/main/java/com/profile/demo/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.profile.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/home")
 public class HomeController {
 
+    @Value("${custom.orig}")
+    private String origstr;
+
+    @Value("${custom.active}")
+    private String activestr;
+
     @GetMapping("/home")
     @ResponseBody
     public String home() {
-        return "this is home/home page";
+        return "this is home/home page: active:"+activestr+";  orig:"+origstr;
     }
 }
